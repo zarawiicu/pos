d2aa0c6fc5ec099ecf39bca98a2be3f4ef29b89c
zarawiicu <zarasaa05@gmail.com>
2024-09-08 22:13:18 +0700

    Upload POS

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\myController;
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+
+Route::get('category/food-beverage', [ProductController::class, 'index'])->name('product.fb');
+Route::get('category/beauty-health', [ProductController::class, 'beauty'])->name('product.beh');
+Route::get('category/home-care', [ProductController::class, 'care'])->name('product.hc');
+Route::get('category/baby-kid', [ProductController::class, 'baby'])->name('product.bk');
+
+
+// Belum bisa menggunakan Route PREFIX karena dalam proggram ini saya belum menambahkan authentifikasi
+// Route::prefix('auth')->group(function(){
+//     Route::get('category/food-beverage', [ProductController::class, 'index'])->name('product.fb');
+//     Route::get('category/beauty-health', [ProductController::class, 'beauty'])->name('product.beh');
+//     Route::get('category/home-care', [ProductController::class, 'care'])->name('product.hc');
+//     Route::get('category/baby-kid', [ProductController::class, 'baby'])->name('product.bk');
+// });
+
+Route::get('/user/{id}/{name}',[myController::class, 'user'])->name('user');
+Route::get('/penjualan', [myController::class, 'penjualan'])->name('penjualan');
