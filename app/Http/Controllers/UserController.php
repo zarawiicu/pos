c3dc4c87d91e83391f6807604b9632772bbcbb9c
zarawiicu <zarasaa05@gmail.com>
2024-09-30 23:14:47 +0700

    Praktikum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,16 +10,23 @@ class UserController extends Controller
 {
     public function index()
 {
-    $user = UserModel::firstOrNew([
-        'username' => 'manager33',
-        'nama' => 'Manager Tiga Tiga',
+    $user = UserModel::create([
+        'username' => 'manager11',
+        'nama' => 'Manager11',
         'password' => Hash::make('12345'),
         'level_id' => 2
     ],
 );
 
+    $user->username = 'manager12';
+
     $user->save();
-    return view('user', ['data' => $user]);
+
+   $user->wasChanged();
+   $user->wasChanged('username');
+   $user->wasChanged(['username','level_id']);
+   $user->wasChanged('nama');
+   dd($user->wasChanged(['nama', 'username']));
 }
 }
 
